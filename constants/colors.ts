a4f5e5a3cec31203ba649e2c88a7ed3f57c4a466
Gabriell-Rodrigues-dos-Santos <gabrielldossantos500@gmail.com>
2026-02-25 16:34:51 -0300

    fase-02 imc

---
+++ constants/colors.ts
@@ -0,0 +1,11 @@
+export const colors = {
+  primary: '#3498db',    // Azul
+  secondary: '#2ecc71',  // Verde
+  danger: '#e74c3c',     // Vermelho
+  dark: '#34495e',       // Cinza escuro
+  light: '#ecf0f1',      // Cinza claro
+  white: '#ffffff',
+  black: '#2c3e50',
+};
+
+export default colors;
